131d08d1deddf682189f4fe5f83a78810f7f482b
Marshall Sosland <marshall.sosland@gmail.com>
2016-04-22 17:07:11 -0500

    Create top level components skeleton

--- app/components/Wrapper.jsx
+++ app/components/Wrapper.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+// import ReactDOM from 'react-dom';
+import Header from './Header.jsx';
+import Instructor from './Instructor.jsx';
+import Workspace from './Workspace.jsx';
+import Footer from './Footer.jsx';
 
 export default class Wrapper extends React.Component {
   render() {
     return (
-      <div>
-        <h1>test</h1>
+      <div id="wrapper">
+        <Header	/>
+        <Instructor	/>
+        <Workspace />
+        <Footer />
       </div>
     );
   }
