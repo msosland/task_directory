1b2b50d66b3739f8062ebb1b09d463ebf49fbc90
Marshall Sosland <marshall.sosland@gmail.com>
2016-04-29 10:00:03 -0500

    Install style-loader and css-loader for CSS modules, add loader to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -24,6 +24,10 @@ module.exports = {
       {
         test: /\.json$/,
         loader: 'json'
+      },
+      {
+        test: /\.css$/,
+        loader: 'style!css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]'
       }
     ]
   }
