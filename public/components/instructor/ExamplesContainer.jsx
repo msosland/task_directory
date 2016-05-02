da969cf178e952f712fe9b646619a3c747490ab6
Marshall Sosland <marshall.sosland@gmail.com>
2016-05-02 16:13:53 -0500

    Refactor image mapping function inside Instructor Examples Container

--- public/components/instructor/ExamplesContainer.jsx
+++ public/components/instructor/ExamplesContainer.jsx
@@ -6,15 +6,11 @@ import styles from '../../css/instructor.css';
 
 export default class ExamplesContainer extends React.Component {
   render() {
-    console.log(this.props.fixture.instructor.images);
-    var exampleImages = this.props.fixture.instructor.images.map(function(exampleImage) {
-      return (
-        <ExampleImage src={exampleImage.src} exampleAnswer={exampleImage.exampleAnswer} correct={exampleImage.correct}/>
-      );
-    });
     return (
       <div className={styles.example}>
-        {exampleImages}
+        {this.props.fixture.instructor.images.map((exampleImage) => {
+          return <ExampleImage src={exampleImage.src} exampleAnswer={exampleImage.exampleAnswer} correct={exampleImage.correct}/>
+        })}
       </div>
     );
   }
