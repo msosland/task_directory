342c2843b1f2603649154d1d181465e39785a970
Marshall Sosland <marshall.sosland@gmail.com>
2016-05-03 09:51:19 -0500

    Add CSS Module and basic style for Footer button

--- public/components/footer/Footer.jsx
+++ public/components/footer/Footer.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 
+import styles from '../../css/footer.css';
+
 export default class Footer extends React.Component {
   render() {
     return (
-      <div id="footer">
-        Footer
+      <div className={styles.footer}>
+        <button className={styles.submitButton}>Submit</button>
       </div>
       );
   }
